a41a0631862d6a3af8fc668d2010fb31a443e906
VladislavLut <143324701+VladislavLut@users.noreply.github.com>
2023-10-16 20:35:44 +0300

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <Windows.h>
+using std::cout;
+using std::endl;
+
+int main()
+{
+	system("chcp 1251");
+	system("cls");
+	cout << "Думи мої, думи мої,\n";
+	cout << "Лихо мені з вами!\n";
+	cout << "Нащо стали на папері\n";
+	cout << "Сумними рядами?..\n";
+	cout << "Чом вас вітер не розвіяв\n";
+	cout << "В степу, як пилину?\n";
+	cout << "Чом вас лихо не приспало,\n";
+	cout << "Як свою дитину?..\n";
+	cout << "Автор: Тарас Григорович Шевченко";
+
+}
