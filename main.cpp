2292d0dc7766e6152affedd54d9b1e5b8a813404
Lutsenko Vladyslav Serhiyovych <143324701+VladislavLut@users.noreply.github.com>
2023-10-23 11:27:50 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,19 @@
 #include <iostream>
-#include <Windows.h>
-using std::cout;
-using std::endl;
+#include <string>
 
-int main()
-{
-	system("chcp 1251");
-	system("cls");
-	cout << "Думи мої, думи мої,\n";
-	cout << "Лихо мені з вами!\n";
-	cout << "Нащо стали на папері\n";
-	cout << "Сумними рядами?..\n";
-	cout << "Чом вас вітер не розвіяв\n";
-	cout << "В степу, як пилину?\n";
-	cout << "Чом вас лихо не приспало,\n";
-	cout << "Як свою дитину?..\n";
-	cout << "Автор: Тарас Григорович Шевченко";
+using namespace std;
 
+int main() {
+    string title = "Bike for sale";
+    string description = "A new bike ideal for daily commuting.";
+    double price = 350.50;
+    string contactInfo = "Tel: (380) 456-7890, E-mail: vlad@email.com";
+    cout << "********** SALE NOTICE **********" << endl;
+    cout << "Named: " << title << endl;
+    cout << "Description: " << description << endl;
+    cout << "Price.: " << price << " UAH" << endl;
+    cout << "Contact information: " << contactInfo << endl;
+    cout << "********************************************" << endl;
+
+    return 0;
 }
